ead08fb7d3c678501531aff050efd9f9b530fe92
Stephen Gordon <stegor115@gmail.com>
2018-12-14 04:21:00 -0500

    Notifications kinda sorta working close enough

--- app/src/main/java/com/example/hydropro/NotificationActivity.java
+++ app/src/main/java/com/example/hydropro/NotificationActivity.java
@@ -9,14 +9,20 @@ import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import java.util.Calendar;
 import android.app.AlarmManager;
+import android.widget.Toast;
 
-public class NotificationActivity extends AppCompatActivity {
+public class NotificationActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener{
 
+    int hour = 0;
+    int minute = 0;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -44,18 +50,37 @@ public class NotificationActivity extends AppCompatActivity {
         setContentView(R.layout.activity_notification);
 
         final Button bSave = findViewById(R.id.buttonSave);
+        final Spinner dropDownHours = findViewById(R.id.dropdown_hour);
+        Spinner dropDownMins = findViewById(R.id.dropdown_minute);
+
+        ArrayAdapter<CharSequence> hoursAdapter = ArrayAdapter.createFromResource(this,R.array.hours,android.R.layout.simple_spinner_item);
+        hoursAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        dropDownHours.setAdapter(hoursAdapter);
+        dropDownHours.setSelection(0);
+        dropDownHours.setOnItemSelectedListener(this);
+
+        ArrayAdapter<CharSequence> minsAdapter = ArrayAdapter.createFromResource(this,R.array.minutes,android.R.layout.simple_spinner_item);
+        minsAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        dropDownMins.setAdapter(minsAdapter);
+        dropDownMins.setSelection(0);
+        dropDownMins.setOnItemSelectedListener(this);
 
         bSave.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                hour = Integer.parseInt(dropDownHours.getSelectedItem().toString());
+                minute = Integer.parseInt(dropDownHours.getSelectedItem().toString());
                 Calendar calendar = Calendar.getInstance();
-                calendar.set(Calendar.HOUR_OF_DAY,18);
-                calendar.set(Calendar.MINUTE, 50);
-                calendar.set(Calendar.SECOND, 30);
+                calendar.set(Calendar.HOUR_OF_DAY,hour);
+                calendar.set(Calendar.MINUTE, minute);
+                calendar.set(Calendar.SECOND, 1);
                 Intent intent = new Intent(getApplicationContext(), NotifyReciever.class);
+                //intent.setAction("MY_NOTIFICATION_MESSAGE");
                 PendingIntent pendingIntent = PendingIntent.getBroadcast(getApplicationContext(),100,intent, PendingIntent.FLAG_UPDATE_CURRENT);
                 AlarmManager alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
                 alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), AlarmManager.INTERVAL_DAY, pendingIntent);
+
+                startActivity(new Intent(NotificationActivity.this, SettingActivity.class)); //Go back to settings
             }
         });
 
@@ -64,4 +89,14 @@ public class NotificationActivity extends AppCompatActivity {
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
 
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        String text  = parent.getItemAtPosition(position).toString();
+        Toast.makeText(parent.getContext(), text, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
 }
